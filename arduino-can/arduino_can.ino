070f0fd5bc64809a5afc84dc048595b56a1f8588
hubbsy <johnsoucy@me.com>
2013-05-18 00:29:29 -0400

    fixed sloppy old pullup setups. added display of all received CAN msgs.
    This needs to be loaded into the arduino and tested.

--- arduino-can/arduino_can.ino
+++ arduino-can/arduino_can.ino
@@ -26,7 +26,8 @@ byte can_receive_buffer[8]; // storage area for received CAN messages
 unsigned short transmit_message_id = 99; // CAN message ID used in messages sent to the CAN bus from this program
 byte received_message_length = 0; // length of received message (number of bytes)
 unsigned short received_message_id = 0; // CAN message ID from received message
-unsigned long display_delay = 0; // time to keep received message on LCD screen before clearing
+unsigned long display_delay_duration = 2000; // time in milliseconds to keep a received CAN message and info on LCD screen before clearing
+unsigned long message_received_time = 0; // millis() at time of receiving a CAN message
 
 // EMPTY_LINE is declared in the LCD header file
 
@@ -38,19 +39,14 @@ void setup() {
   // CAN-BUS Shield I/O pin assignments
   pinMode(CAN_SHIELD_LED_2_PIN, OUTPUT);
   pinMode(CAN_SHIELD_LED_3_PIN, OUTPUT);
-  pinMode(JOYSTICK_UP_PIN, INPUT);
-  pinMode(JOYSTICK_DOWN_PIN, INPUT);
-  pinMode(JOYSTICK_LEFT_PIN, INPUT);
-  pinMode(JOYSTICK_RIGHT_PIN, INPUT);
-  pinMode(JOYSTICK_CLICK_PIN, INPUT);
   
-  // The CAN-BUS Shield joystick uses true-LOW logic.
+    // REMEMBER: The CAN-BUS Shield joystick uses true-LOW logic.
   // When the pin is read as LOW, then that direction or click is active.
-  digitalWrite(JOYSTICK_UP_PIN, HIGH); // enable internal pull-up resistor on digital input
-  digitalWrite(JOYSTICK_DOWN_PIN, HIGH); // enable internal pull-up resistor on digital input
-  digitalWrite(JOYSTICK_LEFT_PIN, HIGH); // enable internal pull-up resistor on digital input
-  digitalWrite(JOYSTICK_RIGHT_PIN, HIGH); // enable internal pull-up resistor on digital input
-  digitalWrite(JOYSTICK_CLICK_PIN, HIGH); // enable internal pull-up resistor on digital input
+  pinMode(JOYSTICK_UP_PIN, INPUT_PULLUP);
+  pinMode(JOYSTICK_DOWN_PIN, INPUT_PULLUP);
+  pinMode(JOYSTICK_LEFT_PIN, INPUT_PULLUP);
+  pinMode(JOYSTICK_RIGHT_PIN, INPUT_PULLUP);
+  pinMode(JOYSTICK_CLICK_PIN, INPUT_PULLUP);
   
   // setup the LCD screen
   parallax_serial_LCD.begin(19200); // make sure the switches are set to the same baud rate
@@ -96,7 +92,7 @@ void setup() {
 void loop() {
   
   // clear/empty the transmit buffer
-  // clear the LCD buffers
+  
   can_transmit_buffer[0] = 0x00;
   can_transmit_buffer[1] = 0x00;
   can_transmit_buffer[2] = 0x00;
@@ -105,13 +101,16 @@ void loop() {
   can_transmit_buffer[5] = 0x00;
   can_transmit_buffer[6] = 0x00;
   can_transmit_buffer[7] = 0x00;
-  LCD_line_one = EMPTY_LINE;
-  LCD_line_two = EMPTY_LINE;
+  
+  // clear the LCD buffers after the "keep it visible" delay time has transpired
+  if((millis() - message_received_time) >= display_delay_duration){
+    LCD_line_one = EMPTY_LINE;
+    LCD_line_two = EMPTY_LINE;
+  }
   
   // read all of the joystick digital inputs
   if (digitalRead(JOYSTICK_CLICK_PIN) == LOW){
     LCD_line_one = "stick is CLICKED";
-    LCD_line_two = EMPTY_LINE;
     can_transmit_buffer[0] = 8;
   }
   if (digitalRead(JOYSTICK_UP_PIN) == LOW){
@@ -140,18 +139,56 @@ void loop() {
 
   // Check for received messages in the CAN-BUS Shield buffer ZERO
   // if there is data in the CAN Shield buffer, then copy it to can_receive_buffer
-  if((CAN.readStatus() & 0x01) == 0x01){ // 
+  if((CAN.readStatus() & 0x01) == 0x01){
+    message_received_time = millis();
     CAN.readDATA_ff_0(&received_message_length, can_receive_buffer, &received_message_id);
+    
+    LCD_line_one = EMPTY_LINE;
     LCD_line_two = EMPTY_LINE;
-    LCD_line_two = "Rx msg id:";
-    LCD_line_two += received_message_id;
+
+    // assemble line one of the LCD
+    LCD_line_one = "id ";
+    LCD_line_one += received_message_id;
+    LCD_line_one += "  len ";
+    LCD_line_one += received_message_length;
+
+    // assemble line two of the LCD
+    LCD_line_two = "msg: ";
+    LCD_line_two += can_receive_buffer[0];
+    LCD_line_two += can_receive_buffer[1];
+    LCD_line_two += can_receive_buffer[2];
+    LCD_line_two += can_receive_buffer[3];
+    LCD_line_two += can_receive_buffer[4];
+    LCD_line_two += can_receive_buffer[5];
+    LCD_line_two += can_receive_buffer[6];
+    LCD_line_two += can_receive_buffer[7];
   }
   
   // Check for received messages in the CAN-BUS Shield buffer ONE
   // if there is data in the CAN Shield buffer, then copy it to can_receive_buffer
   if((CAN.readStatus() & 0x02) == 0x02){
+    message_received_time = millis();
     CAN.readDATA_ff_1(&received_message_length, can_receive_buffer, &received_message_id);
-    LCD_line_two = "caught 0x02";
+    
+    LCD_line_one = EMPTY_LINE;
+    LCD_line_two = EMPTY_LINE;
+
+    // assemble line one of the LCD
+    LCD_line_one = "id ";
+    LCD_line_one += received_message_id;
+    LCD_line_one += "  len ";
+    LCD_line_one += received_message_length;
+
+    // assemble line two of the LCD
+    LCD_line_two = "msg: ";
+    LCD_line_two += can_receive_buffer[0];
+    LCD_line_two += can_receive_buffer[1];
+    LCD_line_two += can_receive_buffer[2];
+    LCD_line_two += can_receive_buffer[3];
+    LCD_line_two += can_receive_buffer[4];
+    LCD_line_two += can_receive_buffer[5];
+    LCD_line_two += can_receive_buffer[6];
+    LCD_line_two += can_receive_buffer[7];
   }
 
   //  write the LCD buffers to the LCD screen
